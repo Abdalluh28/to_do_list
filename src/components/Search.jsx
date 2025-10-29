29088c79022a8accafb57448f3ba1b7467988931
Abdalluh28 <Abdok7496@gmail.com>
2025-10-29 10:40:57 +0300

    edit the search size

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -40,7 +40,7 @@ export default function Search() {
     return (
         <Paper
             component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', maxWidth: 400 }}
         >
             <InputBase
                 sx={{ ml: 1, flex: 1 }}
